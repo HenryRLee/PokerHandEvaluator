0061c32aa3ecc7abdc963097b4409d94bdbcc9cb
Benedikt Óskarsson <benedikto16@ru.is>
2019-10-26 02:01:16 +0100

    Created all hashtables for the algorithm

---
+++ python/src/hash.py
@@ -0,0 +1,30 @@
+from src.dptables import DP, CHOOSE
+
+
+def hash_quinary(q, length, k):
+  sum_numb = 0
+
+  for i in range(0, length):
+    sum_numb += DP[int(q[i])][length - i - 1][k]
+
+    k -= q[i]
+
+    if k <= 0:
+      break
+
+  return sum_numb
+
+def hash_binary(q, length, k):
+  sum_numb = 0
+
+  for i in range(0, length):
+    if int(q[i]):
+      if length - i - 1 >= k:
+        sum_numb += CHOOSE[length - i - 1][k]
+
+      k -= 1
+    
+    if k == 0:
+      break
+
+  return sum_numb
