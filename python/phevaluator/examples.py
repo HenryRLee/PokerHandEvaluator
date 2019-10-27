028671c2ad7f6a0760079b82eb99d06cce351293
Benedikt Óskarsson <benedikto16@ru.is>
2019-10-27 13:46:16 +0000

    Created tests with 1000 examples for each

--- python/phevaluator/examples.py
+++ python/phevaluator/examples.py
@@ -17,16 +17,16 @@ def example1():
   h = 0 * 4 + 0 # 2c
   i = 7 * 4 + 2 # 9h
 
-  p1 = evaluate_cards(a, b, c, d, e, f, g)
-  p2 = evaluate_cards(a, b, c, d, e, h, i)
+  p1 = evaluate_cards(a, b, c, d, e, f, g)  # expected 292
+  p2 = evaluate_cards(a, b, c, d, e, h, i)  # expected 236
 
   print(f'The rank of the hand in player 1 is {p1}')
   print(f'The rank of the hand in player 2 is {p2}')
   print('Player 2 has a stronger hand')
 
 def example2():
-  p1 = evaluate_cards('9c', '4c', '4s', '9d', '4h', 'Qc', '6c')
-  p2 = evaluate_cards('9c', '4c', '4s', '9d', '4h', '2c', '9h')
+  p1 = evaluate_cards('9c', '4c', '4s', '9d', '4h', 'Qc', '6c')  # expected 292
+  p2 = evaluate_cards('9c', '4c', '4s', '9d', '4h', '2c', '9h')  # expected 236
 
   print(f'The rank of the hand in player 1 is {p1}')
   print(f'The rank of the hand in player 2 is {p2}')
