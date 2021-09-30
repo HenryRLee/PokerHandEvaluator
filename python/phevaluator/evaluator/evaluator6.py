3ef8f4e2e1a9d3888279e2ea545145e5dae3540a
azriel1rf <azriel.1rf@gmail.com>
2021-09-30 13:16:02 +0900

    run `$ black .` at ./python/

--- python/phevaluator/evaluator/evaluator6.py
+++ python/phevaluator/evaluator/evaluator6.py
@@ -22,35 +22,36 @@ binaries_by_id = [
 suitbit_by_id = [0x1,  0x8,  0x40,  0x200,] * 13
 # fmt: on
 
+
 def evaluate_6cards(a, b, c, d, e, f):
-  suit_hash = 0
+    suit_hash = 0
 
-  suit_hash += suitbit_by_id[a]
-  suit_hash += suitbit_by_id[b]
-  suit_hash += suitbit_by_id[c]
-  suit_hash += suitbit_by_id[d]
-  suit_hash += suitbit_by_id[e]
-  suit_hash += suitbit_by_id[f]
+    suit_hash += suitbit_by_id[a]
+    suit_hash += suitbit_by_id[b]
+    suit_hash += suitbit_by_id[c]
+    suit_hash += suitbit_by_id[d]
+    suit_hash += suitbit_by_id[e]
+    suit_hash += suitbit_by_id[f]
 
-  if SUITS[suit_hash]:
-    suit_binary = [0] * 4
+    if SUITS[suit_hash]:
+        suit_binary = [0] * 4
 
-    suit_binary[a & 0x3] |= binaries_by_id[a]
-    suit_binary[b & 0x3] |= binaries_by_id[b]
-    suit_binary[c & 0x3] |= binaries_by_id[c]
-    suit_binary[d & 0x3] |= binaries_by_id[d]
-    suit_binary[e & 0x3] |= binaries_by_id[e]
-    suit_binary[f & 0x3] |= binaries_by_id[f]
+        suit_binary[a & 0x3] |= binaries_by_id[a]
+        suit_binary[b & 0x3] |= binaries_by_id[b]
+        suit_binary[c & 0x3] |= binaries_by_id[c]
+        suit_binary[d & 0x3] |= binaries_by_id[d]
+        suit_binary[e & 0x3] |= binaries_by_id[e]
+        suit_binary[f & 0x3] |= binaries_by_id[f]
 
-    return FLUSH[suit_binary[SUITS[suit_hash]-1]]
+        return FLUSH[suit_binary[SUITS[suit_hash] - 1]]
 
-  quinary = [0] * 13
+    quinary = [0] * 13
 
-  quinary[a >> 2] += 1
-  quinary[b >> 2] += 1
-  quinary[c >> 2] += 1
-  quinary[d >> 2] += 1
-  quinary[e >> 2] += 1
-  quinary[f >> 2] += 1
+    quinary[a >> 2] += 1
+    quinary[b >> 2] += 1
+    quinary[c >> 2] += 1
+    quinary[d >> 2] += 1
+    quinary[e >> 2] += 1
+    quinary[f >> 2] += 1
 
-  return NO_FLUSH_6[hash_quinary(quinary, 13, 6)]
+    return NO_FLUSH_6[hash_quinary(quinary, 13, 6)]
