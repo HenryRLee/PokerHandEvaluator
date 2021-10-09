a936dddd066f8b037c4cb9ff66cd8d3bed72bd72
azriel1rf <43790845+azriel1rf@users.noreply.github.com>
2021-10-09 19:20:10 +0900

    Python packaging (#51)
    * add package information
    
    * modify directory structure (only move files)
    
    * adapt reference/import to new directory structure
    
    * restructure Python namespace like C++ implementation. it can be called without
    specefying filename and grouping hashtables into `tables` like `tables.h` in C++.
    
    * lock environment. just execute `poetry install`
    
    * added how to install to `README`
    
    * add command of install this package to ci.yml
    
    * test with verbose

--- python/tests/test_evaluator.py
+++ python/tests/test_evaluator.py
@@ -1,8 +1,8 @@
-import unittest
 import json
 import os
+import unittest
 
-from evaluator.evaluator import evaluate_cards
+from phevaluator import Card, evaluate_cards
 
 CARDS_FILE_5 = os.path.join(os.path.dirname(__file__), "cardfiles/5cards.json")
 CARDS_FILE_6 = os.path.join(os.path.dirname(__file__), "cardfiles/6cards.json")
@@ -19,13 +19,17 @@ class TestEvaluator(unittest.TestCase):
         self.assertLess(p2, p1)
 
     def test_omaha_example(self):
+        # fmt: off
         p1 = evaluate_cards(
-            "4c", "5c", "6c", "7s", "8s", "2c", "9c", "As", "Kd"  # community cards
-        )  # player hole cards
+            "4c", "5c", "6c", "7s", "8s", # community cards
+            "2c", "9c", "As", "Kd",       # player hole cards
+        )
 
         p2 = evaluate_cards(
-            "4c", "5c", "6c", "7s", "8s", "6s", "9s", "Ts", "Js"  # community cards
-        )  # player hole cards
+            "4c", "5c", "6c", "7s", "8s", # community cards
+            "6s", "9s", "Ts", "Js",       # player hole cards
+        )
+        # fmt: on
 
         self.assertEqual(p1, 1578)
         self.assertEqual(p2, 1604)
@@ -47,3 +51,17 @@ class TestEvaluator(unittest.TestCase):
             hand_dict = json.load(read_file)
             for key, value in hand_dict.items():
                 self.assertEqual(evaluate_cards(*key.split()), value)
+
+    def test_evaluator_interface(self):
+        # int, str and Card can be passed to evaluate_cards()
+        p1 = evaluate_cards(1, 2, 3, 32, 48)
+        p2 = evaluate_cards("2d", "2h", "2s", "Tc", "Ac")
+        p3 = evaluate_cards("2D", "2H", "2S", "TC", "AC")
+        p4 = evaluate_cards(Card("2d"), Card("2h"), Card("2s"), Card("Tc"), Card("Ac"))
+        p5 = evaluate_cards(1, "2h", "2S", Card(32), Card("Ac"))
+
+        self.assertEqual(p1, p2)
+        self.assertEqual(p1, p3)
+        self.assertEqual(p1, p4)
+        self.assertEqual(p1, p5)
+
