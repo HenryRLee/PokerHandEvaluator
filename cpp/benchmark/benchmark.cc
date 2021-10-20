285bc143e0dc55c15231654e24bdfecf84e15914
azriel1rf <azriel.1rf@gmail.com>
2021-10-20 22:07:26 +0900

    tuneup sampling in C++

--- cpp/benchmark/benchmark.cc
+++ cpp/benchmark/benchmark.cc
@@ -3,6 +3,7 @@
 #include <set>
 #include <chrono>
 #include <random>
+#include <ranges>
 #include "phevaluator/phevaluator.h"
 #include "benchmark/benchmark.h"
 
@@ -10,11 +11,11 @@ using namespace phevaluator;
 
 static void EvaluateAllFiveCards(benchmark::State& state) {
   for (auto _ : state) {
-    for(int a = 0; a < 48; a++) {
-      for(int b = a + 1; b < 49; b++) {
-        for(int c = b + 1; c < 50; c++) {
-          for(int d = c + 1; d < 51; d++) {
-            for(int e = d + 1; e < 52; e++) {
+    for (int a = 0; a < 48; a++) {
+      for (int b = a + 1; b < 49; b++) {
+        for (int c = b + 1; c < 50; c++) {
+          for (int d = c + 1; d < 51; d++) {
+            for (int e = d + 1; e < 52; e++) {
               EvaluateCards(a, b, c, d, e);
             }
           }
@@ -27,12 +28,12 @@ BENCHMARK(EvaluateAllFiveCards);
 
 static void EvaluateAllSixCards(benchmark::State& state) {
   for (auto _ : state) {
-    for(int a = 0; a < 47; a++) {
-      for(int b = a + 1; b < 48; b++) {
-        for(int c = b + 1; c < 49; c++) {
-          for(int d = c + 1; d < 50; d++) {
-            for(int e = d + 1; e < 51; e++) {
-              for(int f = e + 1; f < 52; f++) {
+    for (int a = 0; a < 47; a++) {
+      for (int b = a + 1; b < 48; b++) {
+        for (int c = b + 1; c < 49; c++) {
+          for (int d = c + 1; d < 50; d++) {
+            for (int e = d + 1; e < 51; e++) {
+              for (int f = e + 1; f < 52; f++) {
                 EvaluateCards(a, b, c, d, e, f);
               }
             }
@@ -46,13 +47,13 @@ BENCHMARK(EvaluateAllSixCards);
 
 static void EvaluateAllSevenCards(benchmark::State& state) {
   for (auto _ : state) {
-    for(int a = 0; a < 46; a++) {
-      for(int b = a + 1; b < 47; b++) {
-        for(int c = b + 1; c < 48; c++) {
-          for(int d = c + 1; d < 49; d++) {
-            for(int e = d + 1; e < 50; e++) {
-              for(int f = e + 1; f < 51; f++) {
-                for(int g = f + 1; g < 52; g++) {
+    for (int a = 0; a < 46; a++) {
+      for (int b = a + 1; b < 47; b++) {
+        for (int c = b + 1; c < 48; c++) {
+          for (int d = c + 1; d < 49; d++) {
+            for (int e = d + 1; e < 50; e++) {
+              for (int f = e + 1; f < 51; f++) {
+                for (int g = f + 1; g < 52; g++) {
                   EvaluateCards(a, b, c, d, e, f, g);
                 }
               }
@@ -67,20 +68,12 @@ BENCHMARK(EvaluateAllSevenCards);
 
 static unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 static std::default_random_engine generator(seed);
-static std::uniform_int_distribution<int> distribution(0, 51);
 
 static std::vector<int> RandomCardSample(int size) {
-  std::vector<int> ret;
-  std::set<int> sample;
-
-  while (sample.size() < size) {
-    int number = distribution(generator);
-    if (sample.find(number) == sample.end()) {
-      ret.push_back(number);
-      sample.insert(number);
-    }
-  }
-
+  constexpr std::ranges::iota_view card_view(0, 52);
+  auto ret = std::vector<int>(size);
+  // must sample from view. if it's sampled from array/vector, return will be sorted in ascending order
+  std::ranges::sample(card_view, ret.begin(), size, generator);
   return ret;
 }
 
@@ -164,7 +157,8 @@ static void EvaluateRandomOmahaCards(benchmark::State& state) {
                          hands[i][7],
                          hands[i][8]);
     }
-  }}
+  }
+}
 BENCHMARK(EvaluateRandomOmahaCards);
 
 BENCHMARK_MAIN();
