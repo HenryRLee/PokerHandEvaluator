70656770d71cf1dc707c3535b702eadebfaff5dc
Henry Lee <lee0906@hotmail.com>
2023-06-22 11:55:27 +1000

    Add PLO6 evaluator

--- cpp/benchmark/benchmark.cc
+++ cpp/benchmark/benchmark.cc
@@ -188,19 +188,44 @@ static void EvaluateRandomPlo5Cards(benchmark::State& state) {
   for (auto _ : state) {
     for (int i = 0; i < SIZE; i++) {
       EvaluatePlo5Cards(hands[i][0],
-                         hands[i][1],
-                         hands[i][2],
-                         hands[i][3],
-                         hands[i][4],
-                         hands[i][5],
-                         hands[i][6],
-                         hands[i][7],
-                         hands[i][8],
-                         hands[i][9]);
+                        hands[i][1],
+                        hands[i][2],
+                        hands[i][3],
+                        hands[i][4],
+                        hands[i][5],
+                        hands[i][6],
+                        hands[i][7],
+                        hands[i][8],
+                        hands[i][9]);
     }
   }
 }
 BENCHMARK(EvaluateRandomPlo5Cards);
 
-BENCHMARK_MAIN();
+static void EvaluateRandomPlo6Cards(benchmark::State& state) {
+  std::vector<std::vector<int>> hands;
+  CardSampler cs{};
 
+  for (int i = 0; i < SIZE; i++) {
+    hands.push_back(cs.sample(11));
+  }
+
+  for (auto _ : state) {
+    for (int i = 0; i < SIZE; i++) {
+      EvaluatePlo6Cards(hands[i][0],
+                        hands[i][1],
+                        hands[i][2],
+                        hands[i][3],
+                        hands[i][4],
+                        hands[i][5],
+                        hands[i][6],
+                        hands[i][7],
+                        hands[i][8],
+                        hands[i][9],
+                        hands[i][10]);
+    }
+  }
+}
+BENCHMARK(EvaluateRandomPlo6Cards);
+
+BENCHMARK_MAIN();
